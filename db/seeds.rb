5f6b0b112756ad9913f20f05f2dbe85d4e8c9692
Elmar <L-mar@inbox.ru>
2022-02-23 22:32:36 +0400

    Fix rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-greeting-1 = Message.create(greeting: 'Salam there')
-greeting-2 = Message.create(greeting: 'Hikya vun')
-greeting-3 = Message.create(greeting: 'Hi there, how are you?')
-greeting-4 = Message.create(greeting: 'I am a developer')
-greeting-5 = Message.create(greeting: 'My name is Elmar')
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+greeting_a = Message.create(greeting: 'Salam there')
+greeting_b = Message.create(greeting: 'Hikya vun')
+greeting_c = Message.create(greeting: 'Hi there, how are you?')
+greeting_d = Message.create(greeting: 'I am a developer')
+greeting_e = Message.create(greeting: 'My name is Elmar')
