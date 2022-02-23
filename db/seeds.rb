da1e7f31b3a215887288eaa0d3f7c369664f44ac
Elmar <L-mar@inbox.ru>
2022-02-23 21:10:26 +0400

    Add 5 messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greeting_1 = Message.create(greeting: "Salam there")
+greeting_2 = Message.create(greeting: "Hikya vun")
+greeting_3 = Message.create(greeting: "Hi there, how are you?")
+greeting_4 = Message.create(greeting: "I am a developer")
+greeting_5 = Message.create(greeting: "My name is Elmar")
