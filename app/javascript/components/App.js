f8ec2fd9ff3280e997d555bf5f3509bd17a7d37a
Elmar <L-mar@inbox.ru>
2022-02-23 22:07:47 +0400

    Modify componenets

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,24 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import PropTypes from "prop-types"
+import configureStore from '../redux/configureStore'
+import Greeting from './Greeting'
+
+const store = configureStore();
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Greeting /> }></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
 
-App.propTypes = {
-  greeting: PropTypes.string
-};
 export default App
