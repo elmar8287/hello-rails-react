1d60942645d90313bcc58889224f276b6e86ad21
Elmar <L-mar@inbox.ru>
2022-02-23 21:16:46 +0400

    Create a static view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static#index'
   namespace :v1, defaults: { format: :json } do
     get 'messages', to: 'messages#index'
   end
