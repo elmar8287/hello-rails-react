05c1803af774e0a966e3ada7b530aa74b82f7914
Elmar <L-mar@inbox.ru>
2022-02-23 21:27:03 +0400

    Run npm install

--- config/application.rb
+++ config/application.rb
@@ -10,15 +10,6 @@ Bundler.require(*Rails.groups)
 
 module HelloRailsReact
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join('node_modules')
   end
-end
+ end
